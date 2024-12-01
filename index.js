b74a3fc7e3d5698382a8c88ea7628154a335d1d4
Vaibhav3045 <121403124+Vaibhav3045@users.noreply.github.com>
2024-12-01 15:54:39 +0530

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 
 const PORT =  5000;
 var corsoption = {
-    origin: 'http://localhost:5174',
+    origin: 'https://fullstackdevelopment-giva.onrender.com',
   };
 
   const app = express();
